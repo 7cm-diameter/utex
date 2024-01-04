64014a51fefde80e6228529c79a6471955b6c44b
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2024-01-04 13:18:37 +0900

    Add support for cli to read yaml

---
+++ utex/clap.py
@@ -0,0 +1,40 @@
+class Config(dict):
+    def __init__(self, path: str) -> None:
+        from yaml import safe_load
+
+        f = open(path, "r")
+        self.__path = path
+        d: dict = safe_load(f)
+        [self.__setitem__(item[0], item[1]) for item in d.items()]
+        f.close()
+
+    def __missing__(self) -> dict:
+        return dict()
+
+    @property
+    def comport(self) -> dict:
+        return self["Comport"]
+
+    @property
+    def experimental(self) -> dict:
+        return self["Experimental"]
+
+    @property
+    def metadata(self) -> dict:
+        return self["Metadata"]
+
+
+class PinoClap(object):
+    def __init__(self):
+        from argparse import ArgumentParser
+
+        self.__parser = ArgumentParser(
+            description="Command line interface for reading the yaml that describes experimental settings."
+        )
+        self.__parser.add_argument(
+            "--yaml", "-y", help="Path to yaml for the experiment.", type=str
+        )
+        self.__args = self.__parser.yaml
+
+    def config(self) -> Config:
+        return Config(self.__args.yaml)
